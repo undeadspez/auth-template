efae72f9a3917d8b5dfae1a7f196f97366827339
Lyubchenko Yuriy <undeadspez@gmail.com>
2018-08-06 19:58:57 +0300

    Implement advanced login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get new" do
     get new_user_url
     assert_response :success
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
@@ -11,4 +12,21 @@ class ActiveSupport::TestCase
   def is_logged_in?
     not session[:user_id].nil?
   end
+
+   # Log in as a particular user.
+   def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'foobar', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+  
 end
